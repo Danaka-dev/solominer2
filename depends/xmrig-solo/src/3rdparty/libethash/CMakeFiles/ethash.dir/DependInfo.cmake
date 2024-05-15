2eb9b66cd38ea6386abefb480b49e87937d6681d
Danaka <danaka-dev@pm.me>
2024-05-15 18:59:25 +0200

    Initial v2.0.beta1 commit

---
+++ depends/xmrig-solo/src/3rdparty/libethash/CMakeFiles/ethash.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/3rdparty/libethash/ethash_internal.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/3rdparty/libethash/CMakeFiles/ethash.dir/ethash_internal.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/3rdparty/libethash/keccakf800.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/3rdparty/libethash/CMakeFiles/ethash.dir/keccakf800.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CL_TARGET_OPENCL_VERSION=200"
+  "CL_USE_DEPRECATED_OPENCL_1_2_APIS"
+  "HAVE_BUILTIN_CLEAR_CACHE"
+  "HAVE_ROTR"
+  "HAVE_SYSLOG_H"
+  "NDEBUG"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "XMRIG_64_BIT"
+  "XMRIG_ALGO_ARGON2"
+  "XMRIG_ALGO_KAWPOW"
+  "XMRIG_ALGO_RANDOMX"
+  "XMRIG_FEATURE_ADL"
+  "XMRIG_FEATURE_API"
+  "XMRIG_FEATURE_AVX2"
+  "XMRIG_FEATURE_BENCHMARK"
+  "XMRIG_FEATURE_CUDA"
+  "XMRIG_FEATURE_ENV"
+  "XMRIG_FEATURE_HTTP"
+  "XMRIG_FEATURE_HWLOC"
+  "XMRIG_FEATURE_MSR"
+  "XMRIG_FEATURE_NVML"
+  "XMRIG_FEATURE_OPENCL"
+  "XMRIG_FEATURE_SSE4_1"
+  "XMRIG_FIX_RYZEN"
+  "XMRIG_JSON_SINGLE_LINE_ARRAY"
+  "XMRIG_MINER_PROJECT"
+  "XMRIG_OS_LINUX"
+  "XMRIG_OS_UNIX"
+  "XMRIG_STRICT_OPENCL_CACHE"
+  "XMRIG_VAES"
+  "_FILE_OFFSET_BITS=64"
+  "_GNU_SOURCE"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src/3rdparty/libethash/../.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ depends/xmrig-solo/src/3rdparty/libethash/CMakeFiles/ethash.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ethash.dir/ethash_internal.c.o"
+  "CMakeFiles/ethash.dir/keccakf800.c.o"
+  "libethash.a"
+  "libethash.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ethash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
