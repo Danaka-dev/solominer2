1c10bceb7f7bc9bcd5d304cba9f0176cf2634d22
Danaka <danaka-dev@pm.me>
2024-05-15 18:58:24 +0200

    Initial v2.0.beta1 commit

---
+++ depends/xmrig-solo/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/DependInfo.cmake
@@ -0,0 +1,126 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_blake.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_blake.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_bmw.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_bmw.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_cubehash.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_cubehash.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_echo.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_echo.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_fugue.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_fugue.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_groestl.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_groestl.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_hamsi.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_hamsi.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_jh.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_jh.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_keccak.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_keccak.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_luffa.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_luffa.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_sha2.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_sha2.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_shabal.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_shabal.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_shavite.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_shavite.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_simd.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_simd.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_skein.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_skein.c.o"
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/sph_whirlpool.c" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/sph_whirlpool.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CL_TARGET_OPENCL_VERSION=200"
+  "CL_USE_DEPRECATED_OPENCL_1_2_APIS"
+  "HAVE_BUILTIN_CLEAR_CACHE"
+  "HAVE_ROTR"
+  "HAVE_SYSLOG_H"
+  "NDEBUG"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "XMRIG_64_BIT"
+  "XMRIG_ALGO_ARGON2"
+  "XMRIG_ALGO_GHOSTRIDER"
+  "XMRIG_ALGO_KAWPOW"
+  "XMRIG_ALGO_RANDOMX"
+  "XMRIG_FEATURE_ADL"
+  "XMRIG_FEATURE_API"
+  "XMRIG_FEATURE_AVX2"
+  "XMRIG_FEATURE_BENCHMARK"
+  "XMRIG_FEATURE_CUDA"
+  "XMRIG_FEATURE_ENV"
+  "XMRIG_FEATURE_HTTP"
+  "XMRIG_FEATURE_HWLOC"
+  "XMRIG_FEATURE_MSR"
+  "XMRIG_FEATURE_NVML"
+  "XMRIG_FEATURE_OPENCL"
+  "XMRIG_FEATURE_SSE4_1"
+  "XMRIG_FIX_RYZEN"
+  "XMRIG_JSON_SINGLE_LINE_ARRAY"
+  "XMRIG_MINER_PROJECT"
+  "XMRIG_OS_LINUX"
+  "XMRIG_OS_UNIX"
+  "XMRIG_STRICT_OPENCL_CACHE"
+  "XMRIG_VAES"
+  "_FILE_OFFSET_BITS=64"
+  "_GNU_SOURCE"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src/crypto/ghostrider/."
+  "src/crypto/ghostrider/../.."
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/ghostrider.cpp" "/home/god/Documents/Trunks/Gaia/miner/xmrig/xmrig-6.21.0/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/ghostrider.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CL_TARGET_OPENCL_VERSION=200"
+  "CL_USE_DEPRECATED_OPENCL_1_2_APIS"
+  "HAVE_BUILTIN_CLEAR_CACHE"
+  "HAVE_ROTR"
+  "HAVE_SYSLOG_H"
+  "NDEBUG"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "XMRIG_64_BIT"
+  "XMRIG_ALGO_ARGON2"
+  "XMRIG_ALGO_GHOSTRIDER"
+  "XMRIG_ALGO_KAWPOW"
+  "XMRIG_ALGO_RANDOMX"
+  "XMRIG_FEATURE_ADL"
+  "XMRIG_FEATURE_API"
+  "XMRIG_FEATURE_AVX2"
+  "XMRIG_FEATURE_BENCHMARK"
+  "XMRIG_FEATURE_CUDA"
+  "XMRIG_FEATURE_ENV"
+  "XMRIG_FEATURE_HTTP"
+  "XMRIG_FEATURE_HWLOC"
+  "XMRIG_FEATURE_MSR"
+  "XMRIG_FEATURE_NVML"
+  "XMRIG_FEATURE_OPENCL"
+  "XMRIG_FEATURE_SSE4_1"
+  "XMRIG_FIX_RYZEN"
+  "XMRIG_JSON_SINGLE_LINE_ARRAY"
+  "XMRIG_MINER_PROJECT"
+  "XMRIG_OS_LINUX"
+  "XMRIG_OS_UNIX"
+  "XMRIG_STRICT_OPENCL_CACHE"
+  "XMRIG_VAES"
+  "_FILE_OFFSET_BITS=64"
+  "_GNU_SOURCE"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src/crypto/ghostrider/."
+  "src/crypto/ghostrider/../.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ depends/xmrig-solo/src/crypto/ghostrider/CMakeFiles/ghostrider.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ghostrider.dir/ghostrider.cpp.o"
+  "CMakeFiles/ghostrider.dir/sph_blake.c.o"
+  "CMakeFiles/ghostrider.dir/sph_bmw.c.o"
+  "CMakeFiles/ghostrider.dir/sph_cubehash.c.o"
+  "CMakeFiles/ghostrider.dir/sph_echo.c.o"
+  "CMakeFiles/ghostrider.dir/sph_fugue.c.o"
+  "CMakeFiles/ghostrider.dir/sph_groestl.c.o"
+  "CMakeFiles/ghostrider.dir/sph_hamsi.c.o"
+  "CMakeFiles/ghostrider.dir/sph_jh.c.o"
+  "CMakeFiles/ghostrider.dir/sph_keccak.c.o"
+  "CMakeFiles/ghostrider.dir/sph_luffa.c.o"
+  "CMakeFiles/ghostrider.dir/sph_sha2.c.o"
+  "CMakeFiles/ghostrider.dir/sph_shabal.c.o"
+  "CMakeFiles/ghostrider.dir/sph_shavite.c.o"
+  "CMakeFiles/ghostrider.dir/sph_simd.c.o"
+  "CMakeFiles/ghostrider.dir/sph_skein.c.o"
+  "CMakeFiles/ghostrider.dir/sph_whirlpool.c.o"
+  "libghostrider.a"
+  "libghostrider.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/ghostrider.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
